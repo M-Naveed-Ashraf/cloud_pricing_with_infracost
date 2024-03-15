dd9d3f2eb3d8bf57e3bf3ccbe337a55464cf215f
m-naveed-ashraf <mahernaveed531@gmail.com>
2024-03-15 15:03:34 -0400

    CloudSQL pricing added

---
+++ GCP/cloudSQL/infracost_cuds_pricing.py
@@ -0,0 +1,91 @@
+import requests
+import json
+import time
+import os
+
+from ..constants import REGIONS
+
+
+def fetch_cuds_prices_from_infracost():
+    """
+    Fetches Commited Unit Discounts (CUDs) prices from the Infracost API and updates the CUDs dictionary.
+    """
+    print("Fetching CUDs prices from Infracost API...")
+    all_regions = REGIONS
+
+    graphql_query = []
+
+    for region in all_regions:
+        updated_region = region.replace("-", "_")
+        alias = f"products_{updated_region}"
+        query = (
+            f"{alias}: products(filter: {{ vendorName: \"gcp\", service: \"Cloud SQL\", "
+            f"region: \"{region}\" "
+            f"}})  "
+            f"{{ productHash vendorName service productFamily region sku  "
+            f"attributes {{ key value }}  "
+            f"prices {{ USD unit description purchaseOption }} "
+            f"}}"
+        )
+
+        graphql_query.append(query)
+
+    batch_query = json.dumps({
+        "query": f"query {{ {' '.join(graphql_query)} }}",
+    })
+
+    api_url = "https://pricing.api.infracost.io/graphql"
+
+    headers = {
+        'X-Api-Key': 'ico-tHZVHJcrKbucJ8Bg5nG0SSJGCIW29N9s',
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(api_url, headers=headers,
+                             data=batch_query, stream=True)
+
+    if response.status_code == 200:
+        all_results = []
+
+        for region in all_regions:
+            updated_region = region.replace("-", "_")
+            alias = f"products_{updated_region}"
+            results_for_alias = response.json().get("data", {}).get(alias, [])
+            all_results.extend(results_for_alias)
+
+        all_cuds = {}
+        for product in all_results:
+            for attribute in product.get("attributes", []):
+                attribute_description = attribute.get("value", '')
+                if "Commitment" in attribute_description:
+                    response_region = product.get("region", "")
+                    usd_value = None
+                    for price in product.get("prices", []):
+                        usd_value = price.get("USD")
+
+                    if response_region not in all_cuds:
+                        all_cuds[response_region] = {}
+
+                    if "1 year" in attribute_description:
+                        all_cuds[response_region]["Commit1Yr"] = usd_value
+
+                    if "3 year" in attribute_description:
+                        all_cuds[response_region]["Commit3Yr"] = usd_value
+
+                    print("Product with CUDs price:", all_cuds)
+
+        # Constructing the file path dynamically
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        # constants_dir = os.path.join(current_dir, 'constants')
+        cuds_file = os.path.join(current_dir, 'cloudSQL_cuds.py')
+
+        with open(cuds_file, 'w') as file:
+            file.write(f"cuds_pricing = {all_cuds} \n")
+
+        print("CUDs prices successfully updated.")
+        time.sleep(30)  # Adding a delay to avoid overloading the API
+    else:
+        print("Error in API Call")
+
+
+fetch_cuds_prices_from_infracost()
